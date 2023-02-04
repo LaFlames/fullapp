1e85474b3c7fa8f3fa6a0cfdc9bac10baaef1cc7
Vitali <vetalichegga@gmail.com>
2023-02-04 23:29:09 +0300

    feat: add router, lazy components

---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const AboutPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    // just for suspense fallback check
+    setTimeout(() => resolve(import('./AboutPage')), 1500)
+}))
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const MainPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    // just for suspense fallback check
+    setTimeout(() => resolve(import('./MainPage')), 1500)
+}))
