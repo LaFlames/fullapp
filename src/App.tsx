f036d6e1f98fe868ec5c55b9471e172c7b647006
Vitali <vetalichegga@gmail.com>
2023-02-05 15:30:37 +0300

    feat: scss, theme configs, theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import useTheme from "./theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to='/'>Главная</Link>
             <Link to='/about'>О сайте</Link>
             <React.Suspense fallback={<div>Loading</div>}>
                 <Routes>
-                    <Route path='about' element={<AboutPageAsync />}/>
+                    <Route path='/about' element={<AboutPageAsync />}/>
                     <Route path='/' element={<MainPageAsync />}/>
                 </Routes>
             </React.Suspense>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from './ThemeContext';
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || "light";
+
+const ThemeProvider: React.FC = ({children}) => {
+    const [theme, setTheme] = React.useState<Theme>(defaultTheme)
+
+    return (
+        <ThemeContext.Provider value={{
+            theme: theme,
+            setTheme: setTheme
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
