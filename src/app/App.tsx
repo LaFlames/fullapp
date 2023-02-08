5d77800c559482c826de2eeb0a4b2fc982cfcb9b
Vitali <vetalichegga@gmail.com>
2023-02-08 23:41:35 +0300

    feat: navbar, app link, themes for links

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from './providers/ThemeProvider';
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
@@ -11,10 +12,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to='/'>Главная</Link>
-            <Link to='/about'>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>Toggle</button>
         </div>
     );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,11 @@
 type Mods = Record<string, boolean | string>;
 
-export const classNames = (cls: string, mods: Mods, additional: string[]): string => {
+export const classNames = (cls: string, mods?: Mods, additional?: string[]): string => {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         Object.entries(mods)
-            .filter(([_, value]) => value)
+            .filter(([_, value]) => Boolean(value))
             .map(([cls]) => cls)
     ].join(' ').trim()
 };
