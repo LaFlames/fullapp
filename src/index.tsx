dd3306ca4b20d469017e4811fdaeefe1820d686c
Vitali <vetalichegga@gmail.com>
2023-02-19 21:02:27 +0300

    feat: error boundary testing

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import {render} from 'react-dom';
 import App from './app/App';
 import {BrowserRouter} from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-
 import './shared/config/i18n/i18n';
+import {PageError} from 'widgets/PageError';
+import {ErrorBoundary} from 'react-error-boundary';
+import React from 'react';
 
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary FallbackComponent={PageError} >
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById('root')
 );
